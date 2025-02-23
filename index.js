9bab49ba879cb8eccbd5695b1787390b2a3adaf9
Anwesha Adhikari <anweshaa002@gmail.com>
2025-02-23 13:08:41 +0530

    Update index.js
    fixed code

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let page = 1;
+
+        let page = 1;
         let fetching = false;
         const container = document.getElementById('container');
         const cols = Array.from(container.getElementsByClassName('col'));
-        console.log(cols)
 
         const fetchImageData = async () => {
             try {
                 fetching = true;
                 document.getElementById('loader').style.display = 'block';
-                const response = await fetch(`https://dog.ceo/api/breeds/image/random/30`);
+                const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=50`);
                 const data = await response.json();
                 fetching = false;
-                return data.message;
+                return data.map(img => `https://picsum.photos/${Math.floor(Math.random() * 200 + 200)}/${Math.floor(Math.random() * 200 + 200)}`);
             } catch (error) {
                 console.error("Error fetching data:", error);
                 fetching = false;
@@ -19,24 +19,12 @@ let page = 1;
             }
         };
 
-        fetchImageData().then((images) => {
-            if (images.length > 0) {
-                images.forEach((image, index) => {
-                    createCard(image, cols[index % cols.length]);
-
-                    console.log(index % cols.length)
-                });
-            }
-        }).catch((error) => {
-            console.error("Error initial fetch:", error);
-        });
-
         const createCard = (image, col) => {
             const card = document.createElement('div');
             card.classList.add('card');
             const img = document.createElement('img');
             img.src = image;
-            img.alt = "Random Dog Image";
+            img.alt = "Random Image";
             img.style.width = "100%";
             img.onerror = function () {
                 this.parentElement.style.display = "none";
@@ -47,7 +35,6 @@ let page = 1;
             card.appendChild(img);
             col.appendChild(card);
         };
-
 
         const handleScroll = () => {
             if (fetching) return;
@@ -71,3 +58,12 @@ let page = 1;
         };
 
         window.addEventListener('scroll', handleScroll);
+        fetchImageData().then((images) => {
+            if (images.length > 0) {
+                images.forEach((image, index) => {
+                    createCard(image, cols[index % cols.length]);
+                });
+            }
+        }).catch((error) => {
+            console.error("Error initial fetch:", error);
+        });
